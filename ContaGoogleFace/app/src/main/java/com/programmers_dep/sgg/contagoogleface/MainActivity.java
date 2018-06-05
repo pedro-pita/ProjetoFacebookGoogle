9a43ac1c970bef2894e4541de74b0bc584e60161
Nopearino <ajmc.p@hotmail.com>
2018-06-05 09:55:37 +0100

    05/06/2018
    *Adicionar Projeto

---
+++ ContaGoogleFace/app/src/main/java/com/programmers_dep/sgg/contagoogleface/MainActivity.java
@@ -0,0 +1,13 @@
+package com.programmers_dep.sgg.contagoogleface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
